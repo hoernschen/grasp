01547e7ea42fce4642a27cbdafd5a4c6ee8382c7
Abel Wilhelm <abel.wilhelm@capgemini.com>
2024-04-06 20:38:16 +0200

    ADDED: new folders and migrated the consts to the new style

---
+++ src/lib/impact/migration/index.ts
@@ -0,0 +1,48 @@
+import { ConfigParams } from '../../types/general';
+import { PluginInterface, PluginParams } from '../../types/interface';
+import { validateConfig, validateInput } from '../../validation';
+
+export const Bananas = (globalConfig: ConfigParams): PluginInterface => {
+  const metadata = {
+    kind: 'execute',
+  };
+
+  /**
+   * Execute's strategy description here.
+   */
+  const execute = async (
+    inputs: PluginParams[],
+    config?: ConfigParams
+  ): Promise<PluginParams[]> => {
+    const mergedConfig = Object.assign({}, globalConfig, config);
+    const validatedConfig = validateConfig(mergedConfig);
+
+    return inputs.map(input => {
+      const validatedInput = validateInput(input);
+      const inputAndConfig = Object.assign({}, validatedInput, validatedConfig);
+
+      const units: number = inputAndConfig.units ?? 1;
+      const carbon = inputAndConfig.carbon * units;
+
+      return {
+        ...input,
+        ...bananas(carbon),
+      };
+    });
+  };
+
+  /*
+   * Calculate bananas
+   */
+  const bananas = (carbon: number) => {
+    const gPerBanana = 150;
+    return {
+      bananas: carbon / (1.28 * gPerBanana),
+    };
+  };
+
+  return {
+    metadata,
+    execute,
+  };
+};
